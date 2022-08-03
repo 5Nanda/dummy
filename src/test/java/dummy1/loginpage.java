b455d7f35f9fab7c2a199bee4a4412df3561a18c
5Nanda <109574707+5Nanda@users.noreply.github.com>
2022-08-03 15:07:54 +0530

    Update loginpage.java

--- src/test/java/dummy1/loginpage.java
+++ src/test/java/dummy1/loginpage.java
@@ -3,7 +3,7 @@ package dummy1;
 public class loginpage {
  public static void main(String[] args) {
 	System.out.println("hello");
-	System.out.println("hello bangalore");
+	System.out.println("hello bangaluru");
 }
 
 }
