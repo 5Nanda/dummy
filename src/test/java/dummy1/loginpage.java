4a218be0856b63b07ef30ae44dbae66d6c9e9a23
5Nanda <109574707+5Nanda@users.noreply.github.com>
2022-08-03 15:15:17 +0530

    Update loginpage.java

--- src/test/java/dummy1/loginpage.java
+++ src/test/java/dummy1/loginpage.java
@@ -4,6 +4,7 @@ public class loginpage {
  public static void main(String[] args) {
 	System.out.println("hello");
 	System.out.println("hello bangaluru");
+	 System.out.println("hello bangaluru");
 }
 
 }
